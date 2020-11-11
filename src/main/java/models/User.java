2376003d67bc5ec364b0da65cd50a8e3b3238afa
Kyle Maxwell <kem254@pitt.edu>
2020-11-11 15:54:19 -0500

    Followers/followees

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -13,6 +13,9 @@ public class User {
     public String email;
     public String displayName;
     public ArrayList<Message> messages = new ArrayList<>();
+    public ArrayList<User> followees = new ArrayList<>();
+    public ArrayList<User> followers = new ArrayList<>();
+
     public ObjectId getId() {
         return id;
     }
@@ -48,5 +51,31 @@ public class User {
     public void addMessage(Message message)
     {
         this.messages.add(message);
+    }
+
+    public ArrayList<User> getFollowees() {
+        return followees;
+    }
+
+    public void setFollowees(ArrayList<User> followees) {
+        this.followees = followees;
+    }
+
+    public void addFollowee(User user)
+    {
+        this.getFollowees().add(user);
+    }
+
+    public ArrayList<User> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(ArrayList<User> followers) {
+        this.followers = followers;
+    }
+
+    public void addFollower(User user)
+    {
+        this.getFollowers().add(user);
     }
 }
